dce5d1e974c2cc971eb7767b18b5213eeeaa41a3
Nandita <nandita.roshini@gmail.com>
2026-03-14 16:34:25 +0530

    Added assignments

---
+++ assignment1/assignment1_057.js
@@ -0,0 +1,60 @@
+//1
+// let a = 10;  
+// let b = "20";
+
+// let result = a + b;
+// console.log(result);
+
+
+
+//2
+// let num = -5;
+
+// if (num > 0) {
+//   console.log("Positive number");
+// } else if (num < 0) {
+//   console.log("Negative number");
+// } else {
+//   console.log("Zero");
+// }
+
+
+
+
+//3
+// const student = {
+//   name: "Anil",
+//   rollNo: 23,
+//   marks: 85
+// };
+
+// console.log("Student Name:", student.name);
+// console.log("Marks:", student.marks);
+
+
+
+
+//4x
+// let arr = [10, 20, 30, 40, 50];
+
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
+
+
+
+//5
+// let numbers = [12, 7, 9, 20, 18];
+// let evenCount = 0;
+// let oddCount = 0;
+
+// for (let i = 0; i < numbers.length; i++) {
+//   if (numbers[i] % 2 === 0) {
+//     evenCount++;
+//   } else {
+//     oddCount++;
+//   }
+// }
+
+// console.log("Even numbers:", evenCount);
+// console.log("Odd numbers:", oddCount);
